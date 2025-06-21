c6c48b592faa0ccb24e46e12ece4bd5303bebaa1
abhishek <thefab6company@gmail.com>
2025-06-21 08:10:32 +0000

    size limt

--- next.config.ts
+++ next.config.ts
@@ -18,6 +18,12 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '5mb' // bump as needed
+    }
+  }
 };
+
 
 export default nextConfig;
